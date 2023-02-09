a9bcdae02a923788bc032689b0d9764362900edf
Joseph Price <josephprice747@gmail.com>
2023-02-09 16:51:21 +0000

    added theme code, need to add button to html

--- theme.js
+++ theme.js
@@ -1,1 +1,24 @@
-// add in the code from the local-storage-demo here
+function setTheme() {
+  //change the theme from dark to light and vice versa depending
+
+  if (
+    localStorage.getItem("theme") === "light" ||
+    localStorage.getItem("theme") === "null"
+  ) {
+    localStorage.setItem("theme", "dark");
+    document.body.classList.add("dark");
+  } else {
+    localStorage.setItem("theme", "light");
+    document.body.classList.remove("dark");
+  }
+}
+
+const themeButton = document.getElementById("theme-button");
+themeButton.addEventListener("click", setTheme);
+
+function getTheme() {
+  if (localStorage.getItem("theme") === "dark") {
+    document.body.classList.add("dark");
+  }
+}
+getTheme();
